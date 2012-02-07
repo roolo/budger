e25b03ffc6938317133a7cd69fe123d0b0c150e9
Mailo Svetel <development@rooland.cz>
2012-02-07 12:22:21 +0100

    Devise added for authentication

--- Gemfile
+++ Gemfile
@@ -30,6 +30,8 @@ group :test do
   gem 'spork'
 end
 
+gem 'devise'
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Budger::Application.routes.draw do
+  devise_for :users do
+    get 'sign_in', to: "devise/sessions#new"
+    get 'sign_up', to: "devise/registrations#new"
+  end
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,6 +54,7 @@ Budger::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root :to => 'application#index'
 
   # See how all your routes lay out with "rake routes"
 
